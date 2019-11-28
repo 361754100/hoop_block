d6f812ac1472a4b0f7b0de437de77964e33d52cc
hero <361754100@qq.com>
2019-11-28 15:24:01 +0800

    Initial commit

---
+++ src/main/java/com/hro/hadoop/hoop_block/HoopBlockApplication.java
@@ -0,0 +1,13 @@
+package com.hro.hadoop.hoop_block;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoopBlockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoopBlockApplication.class, args);
+    }
+
+}
